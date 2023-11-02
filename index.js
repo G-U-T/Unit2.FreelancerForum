cc9c122a5812240f95f449e4c2b9b8913107bae3
G-U-T <147336533+G-U-T@users.noreply.github.com>
2023-11-02 15:50:52 -0400

    Came up with some freelancers.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const freelancers = [
+	{ name: "Dr. Ummer", occupation: "Percussion Teacher", starting_price: 30 },
+	{ name: "Ms. Direction", occupation: "Cartographer", starting_price: 40 },
+	{ name: "C.S. Ess", occupation: "Front-End Developer", starting_price: 50 },
+	{ name: "Mr. E", occupation: "Unknown..?", starting_price: 1000 },
+]
+
 const horizontalFlex = document.getElementById("horizontal-flex");
 console.log(horizontalFlex);
